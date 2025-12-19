fa23386189e109b1f1d8e2a1c3e6a0a485aea227
Anmol Singh <anmolsingh7442@gmail.com>
2025-12-20 01:02:22 +0530

    refactor: format subscription routes for improved readability

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,7 +1,31 @@
 import { Router } from "express";
 
-const subscriptionRouter  = Router();
+const subscriptionRouter = Router();
 
- subscriptionRouter.get('/', (req, res) => res.send({ title: "Create new Subscription" }));
+subscriptionRouter.get("/", (req, res) =>
+  res.send({ title: "Create new Subscription" })
+);
+subscriptionRouter.post("/", (req, res) =>
+  res.send({ title: "Create new Subscription" })
+);
+subscriptionRouter.put("/:id", (req, res) =>
+  res.send({ title: `Update Subscription with ID: ${req.params.id}` })
+);
+subscriptionRouter.delete("/:id", (req, res) =>
+  res.send({ title: `Delete Subscription with ID: ${req.params.id}` })
+);
+subscriptionRouter.get("/:id", (req, res) =>
+  res.send({ title: `Subscription Details for ID: ${req.params.id}` })
+);
+subscriptionRouter.get("/user/:id", (req, res) =>
+  res.send({ title: `Subscriptions for User ID: ${req.params.id}` })
+);
+subscriptionRouter.put("/:id/cancel", (req, res) =>
+  res.send({ title: `Cancel Subscription with ID: ${req.params.id}` })
+);
+subscriptionRouter.get('/upcoming-renewals', (req, res) =>
+  res.send({ title: "Upcoming Renewals" })
+);
 
- export default subscriptionRouter;
+
+export default subscriptionRouter;
