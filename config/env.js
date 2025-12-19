43c344be42585aa644b5f6e0ef7dd24c69fb686a
Anmol Singh <anmolsingh7442@gmail.com>
2025-12-20 02:31:19 +0530

    feat: configure environment variables and update server to use dynamic PORT

--- config/env.js
+++ config/env.js
@@ -1,4 +1,4 @@
 import { config } from "dotenv";
 config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
 
-export const PORT = process.env.PORT || 3000;
+export const {PORT, NODE_ENV, DB_URI} = process.env;
